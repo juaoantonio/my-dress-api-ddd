48305bf8b4a5c1753163d4cc3a774512ad24844b
João Barbosa <joaobarbosadev@gmail.com>
2024-11-06 15:58:01 -0300

    docs: add docs to update payment dto

--- src/nest-modules/booking-module/dto/update-payment.dto.ts
+++ src/nest-modules/booking-module/dto/update-payment.dto.ts
@@ -1,6 +1,11 @@
 import { IsNumber, IsPositive } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class UpdatePaymentInputDto {
+  @ApiProperty({
+    description: "Quantidade a ser paga",
+    example: 100,
+  })
   @IsPositive()
   @IsNumber()
   amount: number;
